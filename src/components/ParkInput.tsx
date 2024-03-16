f1a7fcccc185e806490b9148d00f82b6cdd8a138
mohammadnoorainASPL <mohammadnoorain@accuraticssoftwares.com>
2024-03-17 02:45:31 +0530

    add_ho

---
+++ src/components/ParkInput.tsx
@@ -0,0 +1,14 @@
+import { IonPage } from "@ionic/react";
+import React from "react";
+import "./ParkInput.scss";
+
+const ParkInput = () => {
+
+    return (
+        <h1 className={"sdf"}>
+            ParkInput
+        </h1>
+    )
+}
+
+export default ParkInput
