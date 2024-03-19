a4f310588c19e963f35164d5a63098ebafd83402
Anne12503 <kumardheeraj12504@gmail.com>
2024-03-20 02:45:10 +0530

    I am addedSignUp form

--- src/pages/auth/signUp/signUp.tsx
+++ src/pages/auth/signUp/signUp.tsx
@@ -1,41 +1,68 @@
-import { IonBackdrop, IonButton, IonInput, IonLoading, IonPage, IonSpinner } from "@ionic/react";
-import "./SignUp.scss"
+import {
+  IonBackdrop,
+  IonButton,
+  IonInput,
+  IonLoading,
+  IonPage,
+  IonSpinner,
+} from "@ionic/react";
+import "./SignUp.scss";
 import { useState } from "react";
 import ParkInput from "../../../components/ParkInput";
 
 interface SignUpForm {
-    fullname: string,
-    username: string,
-    email: string,
-    password: string,
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
 }
 const SignUp: React.FC = () => {
+  const [form, setForm] = useState<SignUpForm>({
+    fullname: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loaderFor, setLoaderFor] = useState<"submit">();
 
-    const [form, setForm] = useState<SignUpForm>({
-        fullname: '',
-        username: '',
-        email: '',
-        password: '',
+  const handleInputChange = (e: React.FormEvent<HTMLIonInputElement>) => {
+    setForm({
+      ...form,
+      [e.currentTarget.name]: e.currentTarget.value,
     });
-    const [loaderFor, setLoaderFor] = useState<"submit">()
+  };
 
-    const handleInputChange = (e: React.FormEvent<HTMLIonInputElement>) => {
-        setForm({
-            ...form,
-            [e.currentTarget.name]: e.currentTarget.value
-        });
-    }
+  const signUp = () => {
+    setLoaderFor("submit");
+  };
 
-    const signUp = () => {
-        setLoaderFor("submit");
-    }
-
-    return (
-        <IonPage className="SignUp p-5 gap-2">
-            <h2 className="">Sign Up to <b>PARKCAR</b></h2>
-            <ParkInput></ParkInput>
-        </IonPage>
-    );
+  return (
+    <IonPage className="SignUp p-5 gap-2">
+      <div className="CompletePage">
+      <div className="hero">
+        <h1>Sign Up</h1>
+        <button>Log in</button>
+      </div>
+      <div className="name">
+        <input type="text" placeholder="Name" />
+      </div>
+      <div className="Email">
+         <input type="text" placeholder="Email" />
+      </div>
+      <div className="Password">
+         <input type="text" placeholder="Password" />
+         <button>Show</button>
+      </div>
+      <div className="msg">
+        <p>I would like to receive your newsletter and other promotional information.</p>
+      </div>
+      <div className="SignUpButton">
+        <button className="RealSignUpButton">Sign Up</button>
+        <button className="ForgotPassword">Forgot your password?</button>
+      </div>
+      </div>
+    </IonPage>
+  );
 };
 
-export default SignUp
+export default SignUp;
